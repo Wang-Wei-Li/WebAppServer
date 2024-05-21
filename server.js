a57fff02135d8a611de9faa44263166ffb184bc8
Peter Chen <tzuchunchen1015@gmail.com>
2024-05-21 16:12:23 +0800

    Add get product

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import getResponseCreator from "./response.js";
 import getProductInfoCreator from "./productInfo.js";
+import fs from "fs"
 
 const app = express();
 const PORT = 3000;
@@ -8,8 +9,27 @@ const PORT = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const productsRoute = "./data/products.json";
+const productRoutePrefix = "./data/product-";
+const productRouteSuffix = ".json";
+
 // GET
-app.get("/product", (req, res) => {});
+app.get("/product", (req, res) => {
+  const products = JSON.parse(fs.readFileSync(productsRoute));
+  const responseCreator = getResponseCreator();
+
+  responseCreator.setIsSuccess(true);
+
+  let productInfos = [];
+  for(const productId of products) {
+    const productInfo = JSON.parse(fs.readFileSync(productRoutePrefix + productId + productRouteSuffix));
+    productInfos.push(productInfo);
+  }
+  responseCreator.setProductInfos(productInfos);
+
+  const result = responseCreator.getResponse();
+  res.send(result);
+});
 
 app.get("/product/:id", (req, res) => {});
 
